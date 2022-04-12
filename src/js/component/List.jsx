df2e949ad116a9341e0fba24f1d614388d08046a
Emmanuel Leyan <emmanuel.leyan@gmail.com>
2022-04-12 18:07:12 +0000

    meta tags updated

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -6,7 +6,14 @@ const List = (props) => {
 		<div className="container-fluid">
 			<div className="row justify-content-center">
 				<div className="col-md-4">
-					<div>{props.todo}</div>
+					<div className="row justify-content-between">
+						{props.todo}
+						<span
+							className="btn btn-danger"
+							onClick={props.deleteTask}>
+							<i className="fas fa-trash-alt"></i>
+						</span>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -15,6 +22,7 @@ const List = (props) => {
 
 List.propTypes = {
 	todo: PropTypes.string,
+	deleteTask: PropTypes.func,
 };
 
 export default List;
--- src/js/component/todoList.jsx
+++ src/js/component/todoList.jsx
@@ -7,6 +7,9 @@ import List from "./List.jsx";
 const TodoList = () => {
 	const [listTodo, setListTodo] = useState([]);
 	const [newTodo, setNewTodo] = useState("");
+	const [urgentTask, setUrgentTask] = useState([]);
+	const [importantTask, setImportantTask] = useState([]);
+	const [normalTask, setnormalTask] = useState([]);
 
 	//Handel events
 
@@ -24,19 +27,19 @@ const TodoList = () => {
 		}
 	};
 
-	//Delete
+	//Delete Task
 
-	const DeleteTask = (IndexToDelete) =>
-		setTasksList(
-			TasksList.filter((Task, TaskIndex) => TaskIndex !== IndexToDelete)
+	const deleteTask = (IndexToDelete) => {
+		const newListTodo = [...listTodo, newTodo];
+		setListTodo(
+			newListTodo.filter((todo, index) => index !== IndexToDelete)
 		);
-
-	console.log({ listTodo });
+	};
 
 	return (
 		<div className="container-fluid">
 			<div className="row justify-content-center">
-				<div className="col-md-4">
+				<div className="col-md-8 col-lg-5">
 					<div className="input-group mb-3 mt-3">
 						<h1 className="input-group justify-content-center">
 							Todo List
@@ -52,8 +55,40 @@ const TodoList = () => {
 							onChange={(e) => setNewTodo(e.target.value)}
 							onKeyPress={handelKeyEnter}
 						/>
+						<select
+							className={
+								props.todo.priority == "normal"
+									? "form-select w-25 m-2 bg-success"
+									: props.todo.priority == "importante"
+									? "form-select w-25 m-2 bg-warning"
+									: "form-select w-25 m-2 bg-danger"
+							}
+							aria-label="Priority"
+							value={props.todo.priority}
+							onChange={(e) => {
+								let oldPriority = props.todo.priority;
+								props.todo.priority = e.target.value;
+
+								props.modifyPriority(
+									props.todo.todo,
+									oldPriority,
+									props.todo.priority,
+									props.id
+								);
+							}}>
+							<option>Select priority</option>
+							<option value="normal" className="bg-success">
+								Normal
+							</option>
+							<option value="urgente" className="bg-danger">
+								Urgente
+							</option>
+							<option value="importante" className="bg-warning">
+								Importante
+							</option>
+						</select>
 						<button
-							className="input-group-text btn btn-primary"
+							className="d-none d-md-block input-group-text btn btn-primary text-wrap"
 							id="basic-addon2"
 							onClick={handelClick}>
 							Add New Task
@@ -61,7 +96,7 @@ const TodoList = () => {
 					</div>
 				</div>
 				{listTodo.map((todo, index) => (
-					<List key={index} todo={todo} />
+					<List key={index} todo={todo} deleteTask={deleteTask} />
 				))}
 			</div>
 		</div>
@@ -70,7 +105,6 @@ const TodoList = () => {
 
 export default TodoList;
 
-//PropTypes
 /**
  * @todo
  * delete todo
